83c5dd866a48891c8f1474e3f888715c49847b71
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-11-12 13:53:10 -0500

    Add global rate limiting
    Added some basic global rate limiting, nothing  fancy just want to make sure its there

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
@@ -7,15 +9,34 @@ import { OenoModule } from './oeno/oeno.module';
 
 @Module({
   imports: [
+    // configure the env file
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+
+    // rate limit access to the server
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        }
+      ]
+    }),
+
+    // other src modules
     AuthModule,
     DatabaseModule,
     OenoModule
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    // apply the rate limiting globally
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
 export class AppModule {}
